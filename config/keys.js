ee07802c306ead36108339c999852a69d4805cae
regre <abdullah.fachmi@gmail.com>
2018-04-27 15:10:12 +0700

    first commit

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mongoURI: 'mongodb://regre:Oke12345@ds259079.mlab.com:59079/db_pusherpoll',
+  pusherAppId: '516565',
+  pusherKey: '551c6a3418de3c46ad8a',
+  pusherSecret: '937a0d4c91e0940845f9',
+  pusherCluster: 'ap1',
+  pusherEncrypted: true
+};
